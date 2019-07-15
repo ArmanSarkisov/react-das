90758c29f941fcc07f1ac0da0114c9ba9c088279
Arman Sarkisov <arman.sarkisov.96@gmail.com>
2019-07-15 14:08:25 +0400

    reg page

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
+import s from './Registration.module.css';
+
 import { } from 'reactstrap';
 
 class Registration extends Component {
+
+
     render() {
         return (
-            <div>
-                Registration
-            </ div>
+            <div className={s.registration}>
+                <div className={s.reg_title}>
+                    <h2>registration</h2>
+                </div>
+                <div className={s.form}>
+                    <form action="#">
+                        <input type="text" placeholder="Name" onChange={this.handleChange} />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button onClick={this.handleChange}>registration</button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
